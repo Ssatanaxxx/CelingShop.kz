d62baba4f64de4807ab25d1153d182246dd8bea1
Anar <147673485+Ssatanaxxx@users.noreply.github.com>
2026-01-19 17:32:07 +0500

    Update text in About section

--- frontend/src/components/Sections/About/About.tsx
+++ frontend/src/components/Sections/About/About.tsx
@@ -49,7 +49,7 @@ const About: React.FC<{ id: string }> = ({ id }) => {
       <div className="container">
         <div className={styles.header}>
           <h2 className={styles.title}>
-            Мы работаем честно и профессионально{" "}
+            Если бы мне дали власть, я бы сделал многое{" "}
             <span className={styles.highlight}>без лишних обещаний</span>
           </h2>
           <p className={styles.subtitle}>
